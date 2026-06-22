9271a5744e18c0c1aa530c3fff1d7f5733839b3f
SUBHADIP CHAULYA <subhadip1.chaulya@stu.adamasuniversity.ac.in>
2026-06-22 20:50:35 +0530

    Time: 39 ms (34.46%), Space: 47 MB (72.53%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,6 +1,6 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        System.gc();
+        //System.gc();
       for(int i=0;i<nums.length;i++){
         for(int j=0;j<i;j++){
             if(nums[i]+nums[j]==target){
