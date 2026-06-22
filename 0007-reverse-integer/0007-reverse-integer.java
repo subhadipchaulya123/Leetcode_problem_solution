fb735b44f70eafecd16171ee73c30b963c0132fd
SUBHADIP CHAULYA <subhadip1.chaulya@stu.adamasuniversity.ac.in>
2026-06-22 20:09:07 +0530

    Time: 1 ms (99.98%), Space: 42.4 MB (79.36%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -1,16 +1,16 @@
 class Solution {
     public int reverse(int x) {
-        long reverse_integer=0;
+        long rev=0;
     while(x != 0){
-        int remainder=x%10;
-       reverse_integer=(reverse_integer*10)+remainder;
+        int rem=x%10;
+       rev=(rev*10)+rem;
        x=x/10;
     }
-    if(reverse_integer<Integer.MIN_VALUE || reverse_integer>Integer.MAX_VALUE ){
+    if(rev>Integer.MAX_VALUE || rev<Integer.MIN_VALUE){
         return 0;
     }
     else{
-        return (int) reverse_integer;
+        return (int) rev;
     }
     }
 }
